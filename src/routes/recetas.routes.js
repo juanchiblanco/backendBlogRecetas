8c0e4836924121a852103666e8776b33989240e6
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-09-06 12:25:52 -0300

    agregue las funciones a las rutas, tests funcionaron perfectamente

--- src/routes/recetas.routes.js
+++ src/routes/recetas.routes.js
@@ -1,11 +1,15 @@
 import { Router } from "express";
 import {
+    borrarReceta,
   crearReceta,
+  editarReceta,
+  leerRecetaPorId,
   leerRecetas,
 } from "../controllers/recetas.controllers.js";
 
 const router = Router();
 
 router.route("/").get(leerRecetas).post(crearReceta);
+router.route('/:id').get(leerRecetaPorId).delete(borrarReceta).put(editarReceta)
 
 export default router;
