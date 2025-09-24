6541170dbf8a8b5a41e720eb2a8393e84f87db91
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-09-24 17:26:26 -0300

    agregue enrutador de la paginacion

--- src/controllers/recetas.controllers.js
+++ src/controllers/recetas.controllers.js
@@ -65,7 +65,7 @@ export const editarReceta = async (req,res) => {
 export const recetasPaginadas = async (req, res) => {
   try {
     const page = parseInt(req.query.page) || 1;
-    const limit = parseInt(req.query.limit) || 10;
+    const limit = parseInt(req.query.limit) || 5;
     const skip = (page - 1) * limit;
 
     const [recetas, total] = await Promise.all([
--- src/routes/recetas.routes.js
+++ src/routes/recetas.routes.js
@@ -5,6 +5,7 @@ import {
   editarReceta,
   leerRecetaPorId,
   leerRecetas,
+  recetasPaginadas,
 } from "../controllers/recetas.controllers.js";
 import validacionReceta from "../middlewares/validarReceta.js";
 import verificarToken from "../middlewares/verificarToken.js";
@@ -12,6 +13,7 @@ import verificarToken from "../middlewares/verificarToken.js";
 const router = Router();
 
 router.route("/").get(leerRecetas).post([verificarToken,validacionReceta], crearReceta);
+router.route("/paginacion").get(recetasPaginadas)
 router
   .route("/:id")
   .get(leerRecetaPorId)
